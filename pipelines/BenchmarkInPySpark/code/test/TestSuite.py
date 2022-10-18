d83f4edd47d3b1bc39db8ef8a3234c2e17f0fba1
anya.bida1@gmail.com <anya.bida1@gmail.com>
2022-10-18 18:33:02 +0000

    added a unit test for the cleanup gem

--- pipelines/BenchmarkInPySpark/code/test/TestSuite.py
+++ pipelines/BenchmarkInPySpark/code/test/TestSuite.py
@@ -1,6 +1,6 @@
 import unittest
 
-
+from test.benchmarkinpyspark.graph.test_Cleanup import *
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
--- pipelines/BenchmarkInPySpark/code/test/__init__.py
+++ pipelines/BenchmarkInPySpark/code/test/__init__.py
@@ -1,2 +1,3 @@
+from .benchmarkinpyspark import *
 from .conftest import *
 from .TestSuite import *
